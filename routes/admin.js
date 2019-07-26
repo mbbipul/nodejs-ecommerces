55d0fdf808fb17bf993661c484f936cd90395dd8
mbbipul <30537029+mbbipul@users.noreply.github.com>
2019-07-27 00:59:10 +0600

    add-product and shop route views with pug engine

--- routes/admin.js
+++ routes/admin.js
@@ -6,18 +6,19 @@ const rootDir = require('../util/path');
 
 const router = express.Router();
 
+const products = [];
+
 // /admin/add-product => GET
-router.get('/add-product', (req,res,next) => {
-    res.sendFile(path.join(rootDir,'views','add-product.html'));
-    
+router.get('/add-product', (req, res, next) => {
+  res.render('add-product', {pageTitle: 'Add product', path: '/admin/add-product'});
+
 });
 
 // /admin/add-product => POST
-router.post('/add-product', (req,res,next) => {
-    console.log(req.body);
-    
-    res.redirect('/');
+router.post('/add-product', (req, res, next) => {
+  products.push({ title: req.body.title });
+  res.redirect('/');
 });
 
-module.exports = router;
-
+exports.routes = router;
+exports.products = products;
