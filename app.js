9772aeafac7709ae2f20ada72aa1fc9123ad9edf
Bipul Mandol <mbbipulmandol.cse3.bu@gmail.com>
2023-03-19 01:19:37 +0600

    chore : add Dockerfile

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
-const path = require('path');
-const express = require('express');
-const bodyParser = require('body-parser');
+const path = require("path");
+const express = require("express");
+const bodyParser = require("body-parser");
 
-const exphbs = require('express-handlebars');
+const exphbs = require("express-handlebars");
 
-const adminData = require('./routes/admin');
-const shopRoutes = require('./routes/shop');
+const adminData = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
 const app = express();
 
 // For pug engine
@@ -14,22 +14,24 @@ const app = express();
 
 // For express-handlesbar engine
 app.engine(
-    'hbs', 
-    exphbs({
-        extname: 'hbs',
-        layoutsDir: 'views/layouts',
-        defaultLayout: 'main-layout'
-    })
+	"hbs",
+	exphbs({
+		extname: "hbs",
+		layoutsDir: "views/layouts",
+		defaultLayout: "main-layout",
+	})
 );
-app.set('view engine', 'hbs');
-app.set('views','views');
+app.set("view engine", "hbs");
+app.set("views", "views");
 
-app.use(bodyParser.urlencoded({extended: false}));
-app.use(express.static(path.join(__dirname,'public')));
-app.use('/admin',adminData.routes);
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
+app.use("/admin", adminData.routes);
 app.use(shopRoutes);
 
-app.use( (req,res,next) => {
-    res.status(404).render('404',{pageTitle: "Page Not Found!"});
+app.use((req, res, next) => {
+	res.status(404).render("404", { pageTitle: "Page Not Found!" });
 });
-app.listen(3001);
+app.listen(3001, () => {
+	console.log("server running in port 3000");
+});
